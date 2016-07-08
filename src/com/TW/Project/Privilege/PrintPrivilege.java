90ae8d329340d1481666cdd0799e94e09227ff13
Whisperoy <774538869@qq.com>
2016-07-08 14:04:37 +0800

    pair03

---
+++ src/com/TW/Project/Privilege/PrintPrivilege.java
@@ -0,0 +1,7 @@
+package com.TW.Project.Privilege;
+
+/**
+ * Created by Administrator on 2016/7/8.
+ */
+public interface PrintPrivilege {
+}
---
+++ src/com/TW/Project/printList/PrintList.java
@@ -0,0 +1,7 @@
+package com.TW.Project.printList;
+
+/**
+ * Created by Administrator on 2016/7/8.
+ */
+public interface PrintList {
+}
